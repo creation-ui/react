149cee9044b0797eef1df3841a975ca9d78bcfcf
Paweł <pawelkrystkiewicz@outlook.com>
2022-12-29 20:46:47 +0100

    chore: remove node_modules folder

--- packages/creation-ui-react/src/components/autocomplete/node_modules/storybook-addon-next/dist/config/preview.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("next/config");
-(0, config_1.setConfig)(process.env.__NEXT_RUNTIME_CONFIG);
--- packages/creation-ui-react/src/components/radio/node_modules/storybook-addon-next/dist/register.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const addons_1 = require("@storybook/addons");
-addons_1.addons.register('storybook-addon-next', () => { });
