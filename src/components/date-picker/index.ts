304e1937a2ba91a9b9c276a9ecbd6d8616770b17
Paweł <pawelkrystkiewicz@outlook.com>
2023-01-15 21:59:42 +0100

    fix: add styled components styles

--- src/components/date-picker/index.ts
+++ src/components/date-picker/index.ts
@@ -1,2 +1,3 @@
 export { default as DatePicker } from './date-picker'
 export * from './date-picker.types'
+export * from './styled'
--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,3 +1,4 @@
 export { default as Icon } from './icon'
 export * from './icon.type'
 export * from './list-of-icons'
+export * from './styled'
