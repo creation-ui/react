97c71290f65a04eecca3691c977104bdce372414
Paweł <pawelkrystkiewicz@outlook.com>
2023-01-15 22:02:43 +0100

    fix: styled components import error

--- src/components/date-picker/index.ts
+++ src/components/date-picker/index.ts
@@ -1,3 +1,2 @@
 export { default as DatePicker } from './date-picker'
 export * from './date-picker.types'
-export * from './styled'
--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,4 +1,3 @@
 export { default as Icon } from './icon'
 export * from './icon.type'
 export * from './list-of-icons'
-export * from './styled'
