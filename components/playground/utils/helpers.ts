bb7bcabbc68131daf01583f9724d884a5f02c9bf
Paweł <pawelkrystkiewicz@outlook.com>
2023-06-15 20:40:47 +0200

    chore: remove false props from display

--- components/playground/utils/helpers.ts
+++ components/playground/utils/helpers.ts
@@ -21,7 +21,7 @@ export const controlsMapper = (
 export const objectToPropsText = (state: any): string[] =>
   Object.entries(state).flatMap(([key, value]) => {
     if (typeof value === 'boolean') {
-      return value ? `${key}` : `${key}={${value}}`
+      return value ? `${key}` : ''
     } else {
       return `${key}={${JSON.stringify(value)}}`
     }
